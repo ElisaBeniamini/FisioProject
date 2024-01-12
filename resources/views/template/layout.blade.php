ef1d18d38cce01b1f453f6872d247b6cff33d49b
ElisaBeniamini <elisaaa8783@gmail.com>
2024-01-12 16:41:43 +0100

    npm-bootstrap-vite-slot--app.css-js-componentLayout

---
+++ resources/views/template/layout.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
+</div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    {{ $slot }}
+
+</body>
+
+</html>
