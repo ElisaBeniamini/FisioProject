64b28954bca5faf703431b88e7bf1746dff9210b
ElisaBeniamini <elisaaa8783@gmail.com>
2024-07-12 10:41:08 +0200

    NavbarResponsive-logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,36 +1,28 @@
-{{-- <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">FisioRich</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+<!-- Spinner Start ( caricamento pagina)
+<div id="spinner"
+    class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
+ Spinner End -->
+<!-- Navbar & Hero Start -->
+<div class="container-fluid position-relative p-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
+        <a href="{{ route('homepage') }}" class="navbar-brand p-0">
+            <img src="/images/Riccardo Palazzolo1.png" width="230px" class="m-3 img-fluid" alt="Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="fa fa-bars"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="{{ route('patologie') }}" class="nav-item nav-link active">Patologie</a>
+                <a href="{{ route('fisioterapia') }}" class="nav-item nav-link">Fisioterapia</a>
+                <a href="{{ route('domicilio') }}" class="nav-item nav-link">Domicilio</a>
+                <a href="{{ route('contatti') }}" class="nav-item nav-link">Contatti</a>
             </div>
         </div>
-    </div>
-</nav> --}}
-
-
-<nav>
-    <div>
-        <!--logo-->
-        <a href="{{ route('homepage') }}">FISIORICH
-            {{-- <img src="" alt="">  da inserire quando avremo il logo --}}
-        </a>
-    </div>
-    <!--argomenti navbar-->
-    <div>
-        <a href="{{ route('patologie') }}">Patologie</a>
-        <a href="{{ route('fisioterapia') }}">Fisioterapia</a>
-        <a href="{{ route('domicilio') }}"> Domicilio</a>
-        <a href="{{ route('contatti') }}">Contatti</a>
-    </div>
-
-
-</nav>
+    </nav>
+</div>
+<!-- Navbar & Hero End -->
